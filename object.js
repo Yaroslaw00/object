3e06b58d3e81fdb0f1e2cdb5b326de8826ff8aed
Yaroslaw00 <yarikost901@gmail.com>
2026-02-10 20:29:46 +0100

    ок

---
+++ object.js
@@ -0,0 +1,55 @@
+// const wallet = {
+//   owner: "1",
+//   currency: "ERO",
+//   amount: 100000,
+
+// minusMoney(i){
+//   if(wallet.amount < i){console.error("помилка")
+//      return}
+
+//   wallet.amount -= i;
+//     alert(Баланс гаманця:${wallet.amount});
+// },
+//   addMoney(sum) {
+//     wallet.amount += sum;
+//     alert(Баланс гаманця:${wallet.amount});
+//   }
+// };
+// const wq = confirm("поповнити гаманець");
+// if (wq) {
+//   const er = Number(prompt("cкількі"));
+//   wallet.addMoney(er);
+//   console.log(wallet);
+// }
+// // wallet.addMoney
+// console.log(wq);
+
+// const qaz = confirm("зняти кошти");
+// if(qaz){
+//   const question = prompt("скільки коштів хочете зняти?");
+//   wallet.minusMoney(Number(question))
+
+// }
+
+// Створіть об'єкт "weather" з властивостями "temperature", "humidity", "windSpeed". Додайте до об'єкту метод, який повертає "true", якщо температура нижче 0 градусів Цельсія, та "false", якщо температура вище або рівна 0 градусів Цельсія. Температуру потрібно отримати з prompt(). Якщо метод повернув "true" вивести повідомлення “температура нижче 0 градусів Цельсія” і навпаки
+
+const weather = {
+  temperature: 11,
+  humidity: 0.7,
+  windSpeed: 3.5,
+  gradCelcius(grad) {
+    if (grad < 0) {
+      return true;
+    } else if (grad == 0) {
+      return "Температура 0 градусів";
+    } else {
+      return false;
+    }
+  },
+};
+const usetemp = Number(prompt("Введіть температуру вашої погоди"));
+if (weather.gradCelcius(usetemp)) {
+  console.log("Температура нище 0 aбо 0");
+} else {
+  console.log("Температура вище 0");
+}
